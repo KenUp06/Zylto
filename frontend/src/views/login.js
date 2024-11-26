4430eb3a00e2ef670bd607ec4eafda11bf06d53d
KenUp06 <kev.arias06@gmail.com>
2024-11-26 13:13:58 -0500

    correcion de carpeta frontend, antes reconocida como submodulo

---
+++ frontend/src/views/login.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import '../views/login.css'
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Error en la autenticación');
+      }
+
+      const data = await response.json();
+      console.log(data); // Aquí puedes manejar la respuesta (ej. redirigir al usuario)
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Iniciar sesión</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Usuario:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit">Ingresar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
