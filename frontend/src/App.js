82f6b2636393ae6c7d46b82e0dd5246ba1ca6159
KenUp06 <kev.arias06@gmail.com>
2024-11-27 22:38:18 -0500

    implementación de bcrypt en el backend y diseño de algunas vistas en el frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './views/login.js';
+import Home from './views/home.js';
 
 function App() {
   return (
       <Router>
           <Routes>
+              <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
           </Routes>
       </Router>
---
+++ frontend/src/views/home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './home.css'; // Archivo de estilos específico para la vista Home
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <div className="logo">
+          <h1>Inventarios</h1>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/about">Acerca de</a></li>
+            <li><a href="/contactus">Contacto</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <div className="main-content">
+          <h2>Bienvenido a la Gestión de Inventarios</h2>
+          <p>Administra tus inventarios de manera eficiente y sencilla.</p>
+          <div className="buttons">
+            <a href="login" className="btn">Iniciar sesión</a>
+            <a href="register" className="btn">Registrarse</a>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <p>&copy; 2024 Inventarios. Todos los derechos reservados.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
